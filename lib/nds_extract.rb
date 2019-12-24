7bb420f90bec203731b709505863129bca1a71ee
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-24 00:22:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,8 +51,7 @@ def movies_with_director_key(name, movies_collection)
   # movie_with_director_name method
   movies = []
   movie_hash = {}
-  d = movie_with_director_name(name, movies_collection)
-  puts d
+  
 end
 
 

6519ed08639a17d09c3128cde4511518a0244e58
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-24 02:32:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -74,6 +74,14 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  result = {}
+
+  collection.each do |movie|
+    studio = movie[:studio]
+
+    if !s
+      
+    end
 end
 
 def movies_with_directors_set(source)
